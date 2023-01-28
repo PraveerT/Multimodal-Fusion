04bc11d0c19d8c6c4ecb74f8ac2846a5b40dde72
PraveerT <33889544+PraveerT@users.noreply.github.com>
2023-01-28 18:54:37 +0400

    Create CustomArchUnimodal.py

---
+++ CustomArchUnimodal.py
@@ -0,0 +1,50 @@
+def build_model_3DCNN(input_shape,dropout):
+    inputs = keras.layers.Input(shape=input_shape)
+    x = inputs
+    x = keras.layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
+    x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
+    x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
+    x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(128, activation="relu")(x)
+    x = keras.layers.Dropout(0.4)(x)
+    x = keras.layers.Dense(1024, activation="relu")(x)
+    x = keras.layers.Dropout(0.4)(x)
+    x = keras.layers.Dense(12, activation="softmax")(x)
+
+    return keras.Model(inputs, x)
+  
+  
+  def transformer_encoder_OA(inputs, head_size, num_heads, ff_dim, dropout=0):
+
+    x = keras.layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = keras.layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(x, x)
+    x = keras.layers.Dropout(dropout)(x)
+    return x + inputs
+
+def build_model_3DCNN_with_ATT(input_shape,dropout):
+    inputs = keras.layers.Input(shape=input_shape)
+    x = inputs
+    x = keras.layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
+    x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
+    x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
+    x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.Reshape((-1,128))(x)
+    for _ in range(4):
+        x = transformer_encoder_OA(x,2048,16,16, 0.1)
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(128, activation="relu")(x)
+    x = keras.layers.Dropout(0.4)(x)
+    x = keras.layers.Dense(1024, activation="relu")(x)
+    x = keras.layers.Dropout(0.4)(x)
+    x = keras.layers.Dense(12, activation="softmax")(x)
+
+    return keras.Model(inputs, x)
