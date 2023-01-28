e034206fd28f1d84a05ab5725d9857ea1789345b
PraveerT <33889544+PraveerT@users.noreply.github.com>
2023-01-28 19:04:47 +0400

    Create Module_metrics.py

---
+++ Module_metrics.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib
+# matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os,shutil
+from tensorflow import keras
+from IPython.display import clear_output #clear_output(wait=True)
+# plt.style.use('dark_background')
+import pickle
+import tensorflow as tf
+from numpy.random import seed
+
+bs=10
+ep=200
+initial_learning_rate = 0.00001
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True)
+
+METRICS = [
+      keras.metrics.TruePositives(name='tp'),
+      keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.TrueNegatives(name='tn'),
+      keras.metrics.FalseNegatives(name='fn'), 
+      keras.metrics.CategoricalAccuracy(name='categorical_accuracy'),
+      keras.metrics.Precision(name='precision'),
+      keras.metrics.Recall(name='recall'),
+      keras.metrics.AUC(name='auc'),
+      keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+]
+
+
+checkpoint_cb = keras.callbacks.ModelCheckpoint(
+    f"{name}.h5", save_best_only=True
+)
+early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_categorical_accuracy", patience=100)
+# model.summary()
+
+model.compile(loss='categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
+    metrics=METRICS)
