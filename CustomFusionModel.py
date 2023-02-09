a2edc9fdaffaebf44cb5f90a95469073b78b2ed0
PraveerT <33889544+PraveerT@users.noreply.github.com>
2023-02-09 15:05:36 +0400

    C

--- CustomFusionModel.py
+++ CustomFusionModel.py
@@ -140,26 +140,20 @@ def C(input_shape,dropout):
 def C_Attention(input_shape,dropout):
     inputs = keras.layers.Input(shape=input_shape)
     x = inputs
+    x = keras.layers.LayerNormalization(epsilon=1e-6)(res)
     x = keras.layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
     x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+    x = keras.layers.LayerNormalization()(x)
     x = keras.layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
     x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
-    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.LayerNormalization()(x)
     x = keras.layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
     x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
-    x = keras.layers.Reshape((-1,x.shape[-1]))(x)
-    for _ in range(4):
-        x = transformer_encoder(x,2048,16,16, 0.1)
-    x = keras.layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    x = keras.layers.Dropout(0.5)(x)
     x = keras.layers.Flatten()(x)
-    x = keras.layers.Dense(128, activation="relu")(x)
-    x = keras.layers.Dropout(0.4)(x)
-    x = keras.layers.Dense(1024, activation="relu")(x)
-    x = keras.layers.Dropout(0.4)(x)
-    x = keras.layers.Dense(12, activation="softmax")(x)
-
+    x = keras.layers.Transformer(num_heads=8, units=64, activation='relu')(x)
+    x = keras.layers.GlobalAveragePooling1D(data_format="channels_first")(x)
     return keras.Model(inputs, x)
-
 
 def EarlyMerge(Model_A,Model_B,Model_C,lr_schedule,METRICS):
     merged = keras.layers.Add(name="MERGE")([Model_A.output,Model_B.output,Model_C.output])
