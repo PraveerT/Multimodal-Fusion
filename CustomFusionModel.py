d974f57db454281e99488e7772afbfb734db858b
PraveerT <33889544+PraveerT@users.noreply.github.com>
2023-02-09 16:04:56 +0400

    Revert "C again"
    This reverts commit a7a6df3cdf88397eb0a65257ddadd9842ab46c03.

--- CustomFusionModel.py
+++ CustomFusionModel.py
@@ -152,6 +152,10 @@ def B(input_shape,dropout):
 def B_Attention(input_shape,dropout):
     inputs = keras.layers.Input(shape=input_shape)
     x = inputs
+<<<<<<< HEAD
+=======
+    x = keras.layers.LayerNormalization(epsilon=1e-6)(res)
+>>>>>>> parent of a7a6df3 (C again)
     x = keras.layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
     x = keras.layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
     x = keras.layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(x)
