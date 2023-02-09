c084a64da57d9a749c65db535687a4ae9c795aed
PraveerT <33889544+PraveerT@users.noreply.github.com>
2023-02-09 16:00:23 +0400

    Revert "last keras"
    This reverts commit 564cec34ec8775fae934de1591df52103efab839.

--- CustomFusionModel.py
+++ CustomFusionModel.py
@@ -118,7 +118,7 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     x = keras.layers.MultiHeadAttention(
         key_dim=head_size, num_heads=num_heads, dropout=dropout
     )(x, x)
-    x = keras.layers.Dropout(dropout)(x)
+    x = layers.Dropout(dropout)(x)
     res = x + inputs
 
     # Feed Forward Part
