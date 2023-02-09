24df61ee56e1101d65046b55be1489de36c81ce5
PraveerT <33889544+PraveerT@users.noreply.github.com>
2023-02-09 14:29:42 +0400

    Added the feedforward part

--- CustomFusionModel.py
+++ CustomFusionModel.py
@@ -60,13 +60,20 @@ def CustomMultiHeadAttention(n_heads, head_size, name=None):
     )
 
 def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
-
-    x = keras.layers.LayerNormalization(epsilon=1e-6)(inputs)
-    x = keras.layers.MultiHeadAttention(
+    # Normalization and Attention
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(
         key_dim=head_size, num_heads=num_heads, dropout=dropout
     )(x, x)
-    x = keras.layers.Dropout(dropout)(x)
-    return x + inputs
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res
 
 def A(input_shape,dropout):
     inputs = keras.layers.Input(shape=input_shape)
